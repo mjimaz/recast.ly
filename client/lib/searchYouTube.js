d4e83f6be684a98b9eed683f2225ff83df08968c
mjimaz <m.aljimaz@gmail.com>
2016-01-28 11:59:26 -0800

    send a get request to youtube

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,18 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var searchText = $('.form-control').val();
+
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q='+searchText+'&fields=etag%2CeventId%2Citems%2Ckind%2CnextPageToken%2CpageInfo%2CprevPageToken%2CtokenPagination%2CvisitorId&key='+window.YOUTUBE_API_KEY,
+  type: 'GET',
+  success: function (data) {
+    console.log('video received:',data);
+  },
+  error: function (data) {
+    console.error('Recast.ly: Failed to receive videos!',data);
+  }
+});
 };
 
 window.searchYouTube = searchYouTube;
