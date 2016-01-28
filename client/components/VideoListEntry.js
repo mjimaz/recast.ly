de9032d0daa42d8279d09831c68ffaa25e3279e3
mjimaz <m.aljimaz@gmail.com>
2016-01-27 16:08:47 -0800

    display videos in the video list

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,13 +1,34 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+// var VideoListEntry = () => (
+//   <div className="video-list-entry">
+//     <div className="media-left media-middle">
+//       <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+//     </div>
+//     <div className="media-body">
+//       <div className="video-list-entry-title">Video Title</div>
+//       <div className="video-list-entry-detail">Video Description</div>
+//     </div>
+//   </div>
+// );
+
+class VideoListEntry extends React.Component{
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return (
+      <div className="video-list-entry">
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+        </div>
+      </div>
+      )
+  }
+};
 
 window.VideoListEntry = VideoListEntry;
